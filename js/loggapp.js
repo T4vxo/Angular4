54d3211fc86e3702d0d103792ebce8e580e71f49
T4vxo <daniel.lundberg@utb.vxo.se>
2015-11-03 13:31:05 +0100

    swapped name on app -> module

--- js/loggapp.js
+++ js/loggapp.js
@@ -3,5 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var app = angular.module("LoggApp",["ui.router"]);
+var module = angular.module("LoggApp",["ui.router"]);
+
+
+
 
